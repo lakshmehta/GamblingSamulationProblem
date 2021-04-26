e0f4e79cd2776f250b72f9b8a3abaaa795936916
unknown <lakshmehta06@gmail.com>
2021-04-26 09:09:05 +0530

    [LAKSH ADDED INITIAL GAMBLING]

--- GamblingSamulationProblem/Program.cs
+++ GamblingSamulationProblem/Program.cs
@@ -7,6 +7,9 @@ namespace GamblingSamulationProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! Welcome to Gambling Samulation");
+            int STAKE = 100;
+            int BET = 1;
+            Console.ReadLine();
         }
     }
 }
